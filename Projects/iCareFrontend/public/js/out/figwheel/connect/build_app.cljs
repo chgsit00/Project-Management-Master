29f64efce67cec405a76ddaedd94f1bcc96b8ffb
Antonio Parrotta <antonio.parrotta@tbparrotta.de>
2018-02-21 17:57:42 +0100

    Project Inhalt gelöscht

--- Projects/iCareFrontend/public/js/out/figwheel/connect/build_app.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-app (:require [figwheel.client] [myfrontend.dev] [myfrontend.core] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/myfrontend.core.mount-root (apply js/myfrontend.core.mount-root x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'myfrontend.core/mount-root' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "app", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
