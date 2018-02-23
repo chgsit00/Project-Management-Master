2e6f59338c15a3da0e226c1eca5addc42fe02c98
Antonio Parrotta <antonio.parrotta@tbparrotta.de>
2018-02-23 19:49:23 +0100

    WeirdTestClass hinzugefügt. Nimmt eine Liste von Points entgegen und manipuliert die X- und Y-Werte. Gibt eine Liste mit geänderten Punkten zurück.

---
+++ Projects/iCareData/src/main/java/iCareData/WeirdTestClass.java
@@ -0,0 +1,33 @@
+package iCareData;
+
+import java.awt.Point;
+import java.util.List;
+import java.util.Random;
+
+public class WeirdTestClass implements iCareDataInterface{
+
+	@Override
+	public List<Point> getNewCoordinates(List<Point> oldCoordinates) {
+		Random randomGenerator = new Random();
+		int randomNumber;
+		for (Point point : oldCoordinates) {
+			randomNumber = randomGenerator.nextInt(1);
+			if(randomNumber == 1){
+				point.x++;
+			}
+			else {
+				point.x--;
+			}
+				
+			randomNumber = randomGenerator.nextInt(1);
+			if(randomNumber == 1){
+				point.y++;
+			}
+			else {
+				point.y--;
+			}
+		}
+		return oldCoordinates;
+	}
+
+}
