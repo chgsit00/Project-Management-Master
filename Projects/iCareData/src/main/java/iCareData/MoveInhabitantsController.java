8e796841afa28a59d3a381c72ac743842ada1db7
Chris <C.Gschrey@gmx.net>
2018-03-13 13:50:34 +0100

    Methote für PUT in Controller eingebaut.

--- Projects/iCareData/src/main/java/iCareData/MoveInhabitantsController.java
+++ Projects/iCareData/src/main/java/iCareData/MoveInhabitantsController.java
@@ -17,4 +17,14 @@ public class MoveInhabitantsController {
 		
 		return inhabitants;
 	}
+	
+	public void updateInhabitant(String id, int hearthRate, Position position, List<String> restrictions, HealthCheck healthCheck) {
+		Inhabitant inhabitant = inhabitants.stream().filter(inh -> inh.getId().equals(id)).findFirst().orElse(null);
+		if(inhabitant != null) {
+			inhabitant.setHealtCheck(healthCheck);
+			inhabitant.setHeartRate(hearthRate);
+			inhabitant.setPosition(position);
+			inhabitant.setRestrictions(restrictions);
+		}
+	}
 }
