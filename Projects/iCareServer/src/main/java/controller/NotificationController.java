081997e2bbefb3a729c5d6c73187b18fc28a1fe0
Chris <C.Gschrey@gmx.net>
2018-03-14 13:22:22 +0100

    POST Schnittstelle für DebugNotification

--- Projects/iCareServer/src/main/java/controller/NotificationController.java
+++ Projects/iCareServer/src/main/java/controller/NotificationController.java
@@ -3,12 +3,17 @@ package controller;
 import java.util.Collection;
 import java.util.Date;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import iCareServer.NotificationContainer;
+import objects.DebugNotification;
 import objects.Notification;
+import objects.Severity;
 
 @CrossOrigin
 @RestController
@@ -22,4 +27,11 @@ public class NotificationController {
 	public Collection<Notification> getNewNotifications(){
 		return NotificationContainer.getCurrentNotifications(new Date()).values();
 	}	
+	
+	@RequestMapping(value = "/debug/notification", method = RequestMethod.POST)
+	public ResponseEntity<?> updateInhabitant(@RequestBody DebugNotification debugNotification) {
+		Notification notification = new Notification(debugNotification.getMessage(), debugNotification.getRoomId(), "", Severity.RED);
+		NotificationContainer.addNotification(notification);
+		return ResponseEntity.noContent().build();
+	}
 }
